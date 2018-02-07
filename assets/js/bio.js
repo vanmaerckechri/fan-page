80309677ad3f4cc65feabd819b6dcbc330473ea5
Christophe Van Maercke <vanmaerckechri@gmail.com>
2018-02-07 20:28:10 +0100

    recherche sur les events touch

--- assets/js/bio.js
+++ assets/js/bio.js
@@ -1,60 +1,70 @@
-(function(){
+(function()
+{
 	var d = document,
 	accordionToggles = d.querySelectorAll('.js-accordion'),
 	setAria,
 	setAccordionAria,
 	switchAccordion,
-  touchSupported = ('ontouchstart' in window),
-  pointerSupported = ('pointerdown' in window);
+  	touchSupported = ('ontouchstart' in window),
+  	pointerSupported = ('pointerdown' in window);
   
-  skipClickDelay = function(e){
-    e.preventDefault();
-    e.target.click();
-  }
+ 	skipClickDelay = function(e)
+    {
+	    e.target.click();
+    }
 
-		setAriaAttr = function(el, ariaType, newProperty){
+	setAriaAttr = function(el, ariaType, newProperty)
+	{
 		el.setAttribute(ariaType, newProperty);
 	};
-	setAccordionAria = function(el1, el2, expanded){
-		switch(expanded) {
-      case "true":
-      	setAriaAttr(el1, 'aria-expanded', 'true');
-      	setAriaAttr(el2, 'aria-hidden', 'false');
-      	break;
-      case "false":
-      	setAriaAttr(el1, 'aria-expanded', 'false');
-      	setAriaAttr(el2, 'aria-hidden', 'true');
-      	break;
-      default:
+
+	setAccordionAria = function(el1, el2, expanded)
+	{
+		switch(expanded)
+		{
+	      	case "true":
+		      	setAriaAttr(el1, 'aria-expanded', 'true');
+		      	setAriaAttr(el2, 'aria-hidden', 'false');
+		      	break;
+      		case "false":
+		      	setAriaAttr(el1, 'aria-expanded', 'false');
+		      	setAriaAttr(el2, 'aria-hidden', 'true');
+		      	break;
+      		default:
 				break;
 		}
     };
     
-//function
-switchAccordion = function(e) {
-  console.log("triggered");
-	e.preventDefault();
-	var thisAnswer = e.target.parentNode.nextElementSibling;
-	var thisQuestion = e.target;
-	if(thisAnswer.classList.contains('is-collapsed')) {
-		setAccordionAria(thisQuestion, thisAnswer, 'true');
-	} else {
-		setAccordionAria(thisQuestion, thisAnswer, 'false');
-	}
-  	thisQuestion.classList.toggle('is-collapsed');
-  	thisQuestion.classList.toggle('is-expanded');
+	//function
+	switchAccordion = function(e)
+	{
+		e.preventDefault();
+		var thisAnswer = e.target.parentNode.nextElementSibling;
+		var thisQuestion = e.target;
+		if(thisAnswer.classList.contains('is-collapsed'))
+		{
+			setAccordionAria(thisQuestion, thisAnswer, 'true');
+		}
+		else
+		{
+			setAccordionAria(thisQuestion, thisAnswer, 'false');
+		}
+	  	thisQuestion.classList.toggle('is-collapsed');
+	  	thisQuestion.classList.toggle('is-expanded');
 		thisAnswer.classList.toggle('is-collapsed');
 		thisAnswer.classList.toggle('is-expanded');
- 	
-  	thisAnswer.classList.toggle('animateIn');
+	  	thisAnswer.classList.toggle('animateIn');
 	};
-	for (var i=0,len=accordionToggles.length; i<len; i++) {
-		if(touchSupported) {
-      accordionToggles[i].addEventListener('touchstart', skipClickDelay, {passive: true});
-    }
-    if(pointerSupported){
-      accordionToggles[i].addEventListener('pointerdown', skipClickDelay, false);
-    }
+	for (var i=0,len=accordionToggles.length; i<len; i++)
+	{
+		if(touchSupported)
+		{
+      		accordionToggles[i].addEventListener('touchstart', skipClickDelay, {passive: true});
+    	}
+    	if(pointerSupported)
+    	{
+      		accordionToggles[i].addEventListener('pointerdown', skipClickDelay, false);
+    	}
     accordionToggles[i].addEventListener('click', switchAccordion, false);
-  }
+  	}
 })();
